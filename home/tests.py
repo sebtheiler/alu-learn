61247c65f7e08a9588d31fcd62b959448605d1bf
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-07-28 17:56:23 -0500

    Added basic testing infrastructure

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Tag, FlashCard, Deck
 
 # Create your tests here.
+def create_deck():
+    return Deck.objects.create()
+
+class DeckModelTests(TestCase):
+    pass
+
+
+def create_flashcard(front_text, back_text):
+    deck = create_deck()
+    return FlashCard.objects.create(deck=deck, front_text=front_text, back_text=back_text)
+
+class FlashCardModelTests(TestCase):
+    def test_string(self):
+        self.assertEqual(str(create_flashcard('Powerhouse of the cell', 'Mitochondria')), 'Powerhouse of the cell  ---  Mitochondria')
+
+
+def create_tag(text):
+    flashcard = create_flashcard('26th President', 'Teddy Roosevelt')
+    return Tag.objects.create(flashcard=flashcard, tag_text=text)
+
+class TagModelTests(TestCase):
+    def test_string(self):
+        self.assertEqual(str(create_tag('Presidents')), 'Presidents')
