85a32ef379190de758a6b72c551a1f6225a51ac0
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-07-28 17:34:48 -0500

    Added basic models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Deck(models.Model):
+    pass
+
+
+class FlashCard(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+
+    front_text = models.TextField()
+    back_text = models.TextField()
+    # audio (front/back)
+    # image (front/back)
+
+    # intervals, dificulty, ...
+
+    def __str__(self):
+        return self.front_text + '  ---  ' + self.back_text
+
+
+class Tag(models.Model):
+    flashcard = models.ForeignKey(FlashCard, on_delete=models.CASCADE)
+    tag_text = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.tag_text
