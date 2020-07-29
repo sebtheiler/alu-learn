8ab902e50e75232d4bcadfbd6434582e68d0d1c2
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-07-29 16:30:39 -0500

    Setup basic admin details

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Deck
+
+class DeckAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields = ['title', 'user__username', 'user__email']
+    class Meta:
+        model = Deck
+
+admin.site.register(Deck, DeckAdmin)
--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
 class Deck(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # todo: maybe allow this to become NULL?
     title = models.CharField(max_length=128)
 
     # starting difficulty, new cards per day, ...
