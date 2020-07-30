8f4a6057683ad3e552ff39f95f67d02d82deffe6
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-07-30 12:21:06 -0500

    Renamed API endpoints

--- alu/urls.py
+++ alu/urls.py
@@ -21,6 +21,6 @@ from decks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
-    path('api/', include('decks.urls')),
+    path('api/decks/', include('decks.urls')),
     path('createdeck/', views.deck_create_view),
 ]
--- decks/urls.py
+++ decks/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_names = 'decks'
 urlpatterns = [
-    path('decks/', views.deck_list_view),
-    path('decks/<int:deck_id>/delete/', views.deck_delete_view),
-    path('decks/<int:deck_id>/', views.deck_detail_view),
+    path('decklist/', views.deck_list_view),
+    path('<int:deck_id>/delete/', views.deck_delete_view),
+    path('<int:deck_id>/', views.deck_detail_view),
 ]
