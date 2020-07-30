eb1b01ba0b806bc01c5609a85cd5329dcd30af10
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-07-30 08:58:07 -0500

    Slightly improved admin backend

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Deck
+from .models import Deck, FlashCard, Tag
+
+# class TagAdmin(admin.TabularInline):
+#     model = Tag
+
+
+class FlashCardAdmin(admin.TabularInline):
+    model = FlashCard
+    # inlines = [TagAdmin]
+
 
 class DeckAdmin(admin.ModelAdmin):
+    inlines = [FlashCardAdmin]
     list_display = ['__str__', 'user']
     search_fields = ['title', 'user__username', 'user__email']
     class Meta:
