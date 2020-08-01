5d9c933cae979dfdb3a485082131b833a179e5d0
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-01 15:20:56 -0500

    Re-committed things?

---
+++ alu-web/src/lookup/decks/index.js
@@ -0,0 +1,9 @@
+import {EditButton, RedirectButton, Deck, DecksList, DecksComponent} from './components';
+
+export {
+    EditButton,
+    RedirectButton,
+    Deck,
+    DecksList,
+    DecksComponent
+};
