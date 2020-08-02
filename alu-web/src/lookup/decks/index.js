b832be1dc9cb540a25d398f73f740660ae0bfa93
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-02 10:56:12 -0500

    Tidy'ed a few things - now working when signed in on Django half
    Despite this, I still can't get it to use the DevAuthentication class and work on the React side. This doesn't matter too much since I have a script for compiling the React and copying the generated files.

--- alu-web/src/lookup/decks/index.js
+++
@@ -1,9 +0,0 @@
-import {EditButton, RedirectButton, Deck, DecksList, DecksComponent} from './components';
-
-export {
-    EditButton,
-    RedirectButton,
-    Deck,
-    DecksList,
-    DecksComponent
-};
