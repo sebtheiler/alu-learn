bf50c7e4a53532f0607cc99e956083fbf913e34b
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-05 09:57:45 -0500

    Added working login, logout, and registration forms

--- alu/urls.py
+++ alu/urls.py
@@ -3,13 +3,25 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from decks import views
+from decks.views import (
+    decks_list_view,
+    decks_detail_view,
+    decks_profile_view,
+)
+from accounts.views import (
+    login_view,
+    logout_view,
+    register_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.decks_list_view),
-    path('<int:deck_id>/', views.decks_detail_view),
-    path('profile/<str:username>/', views.decks_profile_view),
+    path('', decks_list_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('register', register_view),
+    path('<int:deck_id>/', decks_detail_view),
+    path('profile/<str:username>/', decks_profile_view),
     path('api/decks/', include('decks.api.urls')),
 ]
 
