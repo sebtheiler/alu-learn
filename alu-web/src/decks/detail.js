deeca00e624807ae9f1acd870689b0f0a94a8122
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-06 17:21:58 -0500

    Added user's name and username in deck
    I might remove this, or replace it with the deck's title and description and thumbnail

--- alu-web/src/decks/detail.js
+++ alu-web/src/decks/detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {EditButton, RedirectButton} from './buttons';
+import default_profile_pic from '../images/default_profile_pic.jpg';
 
 export function Deck(props) {
   const {deck} = props;
@@ -15,13 +16,32 @@ export function Deck(props) {
     window.location.href = `/${deck.id}`;
   };
 
+  if (deck.length === 0) {
+    // Still not sure why this happens
+    // Will need to investigate further
+    return null;
+  };
+
   return (<div className={className}>
-    <p>{deck.id} - {deck.title}</p>
-    <div className='btn btn-group'>
-      <EditButton deck={deck} />
-      <RedirectButton deck={deck} link={{href: 'https://www.google.com', display: 'Add Cards', target: '_blank'}} />
-      <RedirectButton deck={deck} link={{href: 'https://www.google.com', display: 'Browse', target: '_blank'}} />
-      {isDetail === true ? null : <button className='btn btn-outline-primary mb-4 mr-1' onClick={handleLink}>View</button>}
+    <div className='d-flex'>
+      <div className='mx-1'>
+        {/* {TODO: Load real profile picture, downscale static file to consume less bandwith} */}
+        <img alt={deck.author.first_name + "'s profile picture"} src={default_profile_pic} height='40' width='40'></img>
+      </div>
+      <div className='col-11'>
+        <p>
+          {deck.author.first_name}{' '}
+          {deck.author.last_name}{' '}
+          @{deck.author.username}{' '}
+        </p>
+        <p>{deck.title}</p>
+        <div className='btn btn-group px-0'>
+          <EditButton deck={deck} />
+          <RedirectButton deck={deck} link={{href: 'https://www.google.com', display: 'Add Cards', target: '_blank'}} />
+          <RedirectButton deck={deck} link={{href: 'https://www.google.com', display: 'Browse', target: '_blank'}} />
+          {isDetail === true ? null : <button className='btn btn-outline-primary mb-4 mr-1' onClick={handleLink}>View</button>}
+        </div>
+      </div>
     </div>
   </div>);
 };
