1fe93115be20c3a5037370bf19da21a4bec80caa
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-26 14:19:50 -0500

    Improved explore page
    Added MD + KaTeX rendering and also removed a slider if it had no decks

--- alu-web/src/explore/base.js
+++ alu-web/src/explore/base.js
@@ -31,16 +31,19 @@ export function ExploreComponent(props) {
       </div>
       <ExploreButtonGroup />
       <hr />
-      <div className='mb-5'>
+      <div className={'mb-5' + (decks.EDITOR && decks.EDITOR.length === 0 ? ' d-none' : '')}>
         <h3>Editor's picks</h3>
+        <p className='text-secondary'>Here are some decks our editors thought deserved the spotlight</p>
         <DeckSlider decks={decks.EDITOR} loading={!decksDidSet} />
       </div>
-      <div className='mb-5'>
+      <div className={'mb-5' + (decks.HOT && decks.HOT.length === 0 ? ' d-none' : '')}>
         <h3>Hottest weekly decks</h3>
+        <p className='text-secondary'>Decks that have recieved the most thanks in the past week</p>
         <DeckSlider decks={decks.HOT} loading={!decksDidSet} />
       </div>
-      <div className='mb-5'>
+      <div className={'mb-5' + (decks.TOP && decks.TOP.length === 0 ? ' d-none' : '')}>
         <h3>Top decks of all time</h3>
+        <p className='text-secondary'>Everyone should take a look at these immortal decks!</p>
         <DeckSlider decks={decks.TOP} loading={!decksDidSet} />
       </div>
     </>
