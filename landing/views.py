9d174c6d8de9bc26508e4b1bf67af6c15f22b2e0
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-08-26 12:11:37 -0500

    Improved landing page

--- landing/views.py
+++ landing/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render, redirect
 def landing_page(request, *args, **kwargs):
     # if request.user.is_authenticated:
     #     return redirect('/home/')
-    return render(request, 'landing/landing.html')
+    context = {
+        'alpha_spots_remaining': 200,
+    }
+    return render(request, 'landing/landing.html', context=context)
