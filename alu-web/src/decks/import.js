9b99d302b7dc08fca3a0a7139edfe7d41d677109
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-07 13:33:04 -0400

    Added placeholders for when .apkg and Quizlet uploads are supported

--- alu-web/src/decks/import.js
+++ alu-web/src/decks/import.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 import {apiDeckTextImport} from '../lookup';
 import {errorHandler} from '../utils';
@@ -6,6 +6,8 @@ import {errorHandler} from '../utils';
 export function DeckImportComponent() {
   const fileRef = React.createRef();
   const titleRef = React.createRef();
+
+  const [uploadType, setUploadType] = useState('TXT');
 
   const handleImport = (event) => {
     event.preventDefault();
@@ -28,50 +30,69 @@ export function DeckImportComponent() {
   return (
     <Form onSubmit={handleImport} className='w-50 mx-auto mt-5 text-center'>
       <Form.Group>
-        <Form.Label className='w-50' style={{lineHeight: '15px'}}>
-          <p className='mb-1'>Title of deck</p>
-          <small className='text-secondary w-50 mb-0'>
-            If you enter the name of a deck that already exists, the uploaded contents will be appended to that deck.
-          </small>
-        </Form.Label>
+        {/* For some reason, switching this to a <label> freaks it out */}
+        <p className='mb-0'>Type of Import</p>
         <Form.Control
-          ref={titleRef}
-          type='text'
-          placeholder='My deck'
-          className='w-50 mx-auto text-center'
-          required
-        />
-      </Form.Group>
-      <p className='mb-0'>
-        Select the file to import
-      </p>
-      <Form.Group className='custom-file mb-4 w-50'>
-        <Form.Label
-          className='custom-file-label'
-          htmlFor='txtFileUpload'
-          id='txt-file-label'
+          as='select'
+          className='w-50'
+          defaultValue={uploadType}
+          onChange={(event) => setUploadType(event.target.value)}
+          custom
         >
-          Choose file
-        </Form.Label>
-        <Form.File
-          className='custom-file-input'
-          id='txtFileUpload'
-          name='uploadFile'
-          accept='.txt'
-          ref={fileRef}
-          required
+          <option value='TXT'>Upload from *.txt</option>
+          <option value='APKG'>Upload from *.apkg</option>
+          <option value='QUIZLET'>Upload from Quizlet</option>
+        </Form.Control>
+      </Form.Group>
+      {uploadType === 'TXT' ? <>
+        <Form.Group>
+          <Form.Label className='w-50' style={{lineHeight: '15px'}}>
+            <p className='mb-1'>Title of deck</p>
+            <small className='text-secondary w-50 mb-0'>
+              If you enter the name of a deck that already exists, the uploaded contents will be appended to that deck.
+            </small>
+          </Form.Label>
+          <Form.Control
+            ref={titleRef}
+            type='text'
+            placeholder='My deck'
+            className='w-50 mx-auto text-center'
+            required
+          />
+        </Form.Group>
+        <p className='mb-0'>
+          Select the file to import
+        </p>
+        <Form.Group className='custom-file mb-4 w-50'>
+          <Form.Label
+            className='custom-file-label'
+            htmlFor='txtFileUpload'
+            id='txt-file-label'
+          >
+            Choose file
+          </Form.Label>
+          <Form.File
+            className='custom-file-input'
+            id='txtFileUpload'
+            name='uploadFile'
+            accept='.txt'
+            ref={fileRef}
+            required
 
-          // Update the label to the name of the uploaded file
-          onChange={() => document.getElementById('txt-file-label').innerHTML = fileRef.current.value.replace('C:\\fakepath\\', '')}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Button
-          type='submit'
-          className='w-50 mx-auto'
-          block
-        >Import!</Button>
-      </Form.Group>
+            // Update the label to the name of the uploaded file
+            onChange={() => document.getElementById('txt-file-label').innerHTML = fileRef.current.value.replace('C:\\fakepath\\', '')}
+          />
+        </Form.Group>
+        <Form.Group>
+          <Button
+            type='submit'
+            className='w-50 mx-auto'
+            block
+          >Import!</Button>
+        </Form.Group>
+      </> : 
+      <p>We currently don't support imports of this type. We are working hard to implement this functionality as soon as possible.</p>
+      }
     </Form>
   );
 };
