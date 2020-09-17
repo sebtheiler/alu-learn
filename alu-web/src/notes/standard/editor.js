d82e4ef6d876310583ba9b391a0ffad0b35d552f
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-17 16:03:09 -0400

    Added hyperlinking

--- alu-web/src/notes/standard/editor.js
+++ alu-web/src/notes/standard/editor.js
@@ -1,10 +1,11 @@
 import React, {useCallback, useMemo, useState, useEffect} from 'react';
-import {createEditor, Editor, Transforms} from 'slate';
+import {createEditor, Editor, Transforms, Range} from 'slate';
 import {Slate, Editable, withReact, useSlate} from 'slate-react';
 import {withHistory} from 'slate-history';
 import {errorHandler, useInterval} from '../../utils';
-import {Button, ButtonGroup} from 'react-bootstrap';
+import {Button, ButtonGroup, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import isHotKey from 'is-hotkey';
+import isUrl from 'is-url'
 import {apiNoteDelete, apiNoteDetail, apiNoteUpdate} from '../../lookup';
 import {DeleteModal} from '../buttons';
 
@@ -34,7 +35,7 @@ export function StandardNoteEditor(props) {
   const [didTypeRecently, setDidTypeRecently] = useState(false);
   const [areChanges, setAreChanges] = useState(false);
   const editor = useMemo(
-    () => withHistory(withReact(createEditor())),
+    () => withLinks(withHistory(withReact(createEditor()))),
     []
   );
   
@@ -119,6 +120,7 @@ export function StandardNoteEditor(props) {
             <MarkButton format='underline' label='Underline' />
             <MarkButton format='strikethrough' label='Strikethrough' />
             <MarkButton format='code' label='Code' />
+            <LinkButton />
             <span className='mx-1' />
             <BlockButton format='heading-one' label='H1' />
             <BlockButton format='heading-two' label='H2' />
@@ -239,6 +241,88 @@ const isMarkActive = (editor, format) => {
   return marks ? marks[format] === true : false;
 };
 
+const withLinks = editor => {
+  const { insertData, insertText, isInline } = editor;
+
+  editor.isInline = element => {
+    return element.type === 'link' ? true : isInline(element);
+  };
+
+  editor.insertText = text => {
+    if (text && isUrl(text)) {
+      wrapLink(editor, text);
+    } else {
+      insertText(text);
+    };
+  };
+
+  editor.insertData = data => {
+    const text = data.getData('text/plain');
+
+    if (text && isUrl(text)) {
+      wrapLink(editor, text);
+    } else {
+      insertData(data);
+    };
+  };
+
+  return editor;
+};
+
+const insertLink = (editor, url) => {
+  if (editor.selection) {
+    wrapLink(editor, url);
+  };
+};
+
+const isLinkActive = editor => {
+  const [link] = Editor.nodes(editor, { match: n => n.type === 'link' });
+  return !!link;
+};
+
+const unwrapLink = editor => {
+  Transforms.unwrapNodes(editor, { match: n => n.type === 'link' });
+};
+
+const wrapLink = (editor, url) => {
+  if (isLinkActive(editor)) {
+    unwrapLink(editor);
+  };
+
+  const { selection } = editor;
+  const isCollapsed = selection && Range.isCollapsed(selection);
+  const link = {
+    type: 'link',
+    url,
+    children: isCollapsed ? [{ text: url }] : [],
+  };
+
+  if (isCollapsed) {
+    Transforms.insertNodes(editor, link);
+  } else {
+    Transforms.wrapNodes(editor, link, { split: true });
+    Transforms.collapse(editor, { edge: 'end' });
+  };
+};
+
+const LinkButton = () => {
+  const editor = useSlate();
+
+  return (
+    <Button
+      variant='outline-primary'
+      onClick={event => {
+        event.preventDefault();
+        const url = window.prompt('Enter the URL of the link:');
+        if (!url) return;
+        insertLink(editor, url);
+      }}
+    >
+      Hyperlink
+    </Button>
+  );
+};
+
 export const Element = ({ attributes, children, element }) => {
   switch (element.type) {
     case 'block-quote':
@@ -253,6 +337,24 @@ export const Element = ({ attributes, children, element }) => {
       return <li {...attributes}>{children}</li>
     case 'numbered-list':
       return <ol {...attributes}>{children}</ol>
+    case 'link':
+      return (
+        <OverlayTrigger
+          overlay={
+            <Tooltip className={'button-tooltip text-center'}>
+              <a href={element.url} style={{ color: 'white' }}>
+                {element.url.length > 50 ? element.url.substring(0, 15) + '   ...   ' + element.url.substring(element.url.length - 10, element.url.length) : element.url}
+              </a>
+            </Tooltip>
+          }
+          placement='top'
+          delay={{ show: 20, hide: 550 }}
+        >
+          <a {...attributes} href={element.url}>
+            {children}
+          </a>
+        </OverlayTrigger>
+      );
     default:
       return <p {...attributes}>{children}</p>
   };
