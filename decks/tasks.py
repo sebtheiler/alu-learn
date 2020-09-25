8eff3010b4f30d9c72c7cad7288eefdf9001cc16
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-24 20:22:13 -0400

    Got Django top deck calculation working

--- decks/tasks.py
+++ decks/tasks.py
@@ -1,24 +1,33 @@
+import json
+
 from celery import shared_task
-# from .models import Deck
-from profiles.models import Profile
-from decks.models import StudySessionManager
-import random
 from celery.decorators import periodic_task
 from celery.task.schedules import crontab
+from django.db.models import Count
+from profiles.models import Profile
+
+from decks.models import Deck, StudySessionManager
+
 
 @shared_task
-def midnight_reset(a):
-    # Break the streaks of users who haven't studied today
-    not_studied_profiles = Profile.objects.filter(has_done_cards_today=False)
-    not_studied_profiles.update(current_streak=0)
+def midnight_reset():
+    # # Break the streaks of users who haven't studied today
+    # not_studied_profiles = Profile.objects.filter(has_done_cards_today=False)
+    # not_studied_profiles.update(current_streak=0)
 
-    # Reset all users to not having studied
-    Profile.objects.all().update(has_done_cards_today=False)
+    # # Reset all users to not having studied
+    # Profile.objects.all().update(has_done_cards_today=False)
 
-    # Reset the number of cards each SSM has done
-    StudySessionManager.objects.all().update(new_cards_done_today=0)
+    # # Reset the number of cards each SSM has done
+    # StudySessionManager.objects.all().update(new_cards_done_today=0)
+
+    # Calculate top deck Ids
+    sorted_decks = Deck.objects.annotate(num_thanks=Count('thanks')).order_by('-num_thanks')
+    top_deck_ids = [deck.id for deck in sorted_decks[:5]]
+    with open('top_deck_ids.json', 'w+') as f:
+        f.write(json.dumps(top_deck_ids))
 
 
-@periodic_task(run_every=crontab(minute=0, hour=0))
+@periodic_task(run_every=crontab(minute=16, hour=0))
 def run_midnight_reset():
-    midnight_reset.delay(random.randint(0, 10))
+    midnight_reset.delay()
--- explore/api/views.py
+++ explore/api/views.py
@@ -6,10 +6,13 @@ from decks.models import Deck
 from decks.serializers import DeckSerializer
 from django.db.models import Q
 from django.views.decorators.cache import cache_page
+from django.conf import settings
+import json
 
 EDITOR_PICKS_DECK_IDS = [20, 21, 18, 22]
-TOP_DECK_IDS = [19, 24, 23, 25]
-HOT_DECK_IDS = []
+with open('top_deck_ids.json', 'r') as f:
+    TOP_DECK_IDS = json.loads(f.read())
+# HOT_DECK_IDS = []
 
 def get_decks_from_ids(id_list, public_only=False):
     query = Q(pk__in=id_list)
@@ -28,7 +31,7 @@ def api_explore_lists_view(request, *args, **kwargs):
     data = {
         'EDITOR': get_decks_from_ids(EDITOR_PICKS_DECK_IDS, public_only=True),
         'TOP': get_decks_from_ids(TOP_DECK_IDS, public_only=True),
-        'HOT': get_decks_from_ids(HOT_DECK_IDS, public_only=True),
+        'HOT': [], #get_decks_from_ids(HOT_DECK_IDS, public_only=True),
     }
 
     return Response(data, status=200)
