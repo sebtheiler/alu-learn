1510e748213a639510b313ac449abc66212ea7dd
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-26 11:29:55 -0400

    Slight changes to autonote

--- alu-web/src/autonote/reader.js
+++ alu-web/src/autonote/reader.js
@@ -6,15 +6,18 @@ export function AutoNote(props) {
   const [finished, setFinished] = useState(false);
 
   const text = `
-On the frontier of Armenia towards the south-east is the kingdom of Mosul. It is a very great kingdom, and inhabited by several different kinds of people whom we shall now describe.
-First there is a kind of people called ARAB, and these worship Mahommet. Then there is another description of people who are NESTORIAN and JACOBITE Christians. These have a Patriarch, whom they call the JATOLIC, and this Patriarch creates Archbishops, and Abbots, and Prelates of all other degrees, and sends them into every quarter, as to India, to Baudas, or to Cathay, just as the Pope of Rome does in the Latin countries. For you must know that though there is a very great number of Christians in those countries, they are all Jacobites and Nestorians; Christians indeed, but not in the fashion enjoined by the Pope of Rome, for they come short in several points of the Faith.
-All the cloths of gold and silk that are called muslin are made in this country; and those great Merchants called Mosolins, who carry for sale such quantities of spicery and pearls and cloths of silk and gold, are also from this kingdom.
-There is yet another race of people who inhabit the mountains in that quarter, and are called CURDS. Some of them are Christians, and some of them are Saracens; but they are an evil generation, whose delight it is to plunder merchants.
-[Near this province is another called MUS and MERDIN, producing an immense quantity of cotton, from which they make a great deal of buckram and other cloth. The people are craftsmen and traders, and all are subject to the Mongol King.]    
-`.trim().split('\n');
+Tauris is a great and noble city, situated in a great province called IRAQ, in which are many other towns and villages. But as Tauris is the most noble I will tell you about it.
+The men of Tauris get their living by trade and handicrafts, for they weave many kinds of beautiful and valuable stuffs of silk and gold. The city has such a good position that merchandize is brought thither from India, Baudas, CREMESOR, and many other regions; and that attracts many Latin merchants, especially Genoese, to buy goods and transact other business there; the more as it is also a great market for precious stones. It is a city in fact where merchants make large profits.
+The people of the place are themselves poor creatures; and are a great medley of different classes. There are Armenians, Nestorians, Jacobites, Georgians, Persians, and finally the natives of the city themselves, who are worshippers of Mahommet. These last are a very evil generation; they are known as TAURIZI.] The city is all girt round with charming gardens, full of many varieties of large and excellent fruits.
+Now we will quit Tauris, and speak of the great country of Persia. [From Tauris to Persia is a journey of twelve days.]  
+`.trim().replace('\n\n', '\n').split('\n');
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    const form = event.target;
+
+    form.elements.notes.value = '';
+
     if (selectedPar < text.length - 1) {
       setSelectedPar(selectedPar + 1);
     } else {
@@ -22,9 +25,9 @@ There is yet another race of people who inhabit the mountains in that quarter, a
     };
   };
 
-  return (<div className='container'>
+  return (<div className='container mt-5'>
     <h3 className='text-center'>Content</h3>
-    <div style={{ border: '1px solid gray', padding: '30px', height: '250px', overflow: 'hidden' }}>
+    <div style={{ border: '1px solid gray', padding: '30px', height: '250px', overflow: 'hidden', borderRadius: '5px' }}>
       <p style={{ color: '#e0e0e0' }}>...{text[selectedPar - 1] && text[selectedPar - 1].substr(text[selectedPar - 1].length - 150, text[selectedPar - 1].length)}</p>
       <p style={{ color: '#000000' }}>{text[selectedPar]}</p>
       <p style={{ color: '#e0e0e0' }}>...{text[selectedPar + 1] && text[selectedPar + 1].substr(0, 150)}</p>
