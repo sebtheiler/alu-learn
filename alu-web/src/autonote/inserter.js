a1ba3a1ceb3e1bc6770bbbd3e566a6675d935234
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-27 19:40:19 -0400

    Cleaned up autonote inserter

--- alu-web/src/autonote/inserter.js
+++ alu-web/src/autonote/inserter.js
@@ -1,10 +1,11 @@
-const content = [{"type": "heading-one", "children": [{"text": "Header 1"}]}, {"type": "paragraph", "children": [{"text": "Content 1 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Header 2"}]}, {"type": "paragraph", "children": [{"text": "Content 2 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Another Header 2"}]}, {"type": "paragraph", "children": [{"text": "Text"}]}, {"type": "heading-one", "children": [{"text": "Second Header 1"}]}, {"type": "paragraph", "children": [{"text": "Content 1 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Second Header 2"}]}, {"type": "paragraph", "children": [{"text": "Content 2 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}];
+// const content = [{"type": "heading-one", "children": [{"text": "Header 1"}]}, {"type": "paragraph", "children": [{"text": "Content 1 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Header 2"}]}, {"type": "paragraph", "children": [{"text": "Content 2 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Another Header 2"}]}, {"type": "paragraph", "children": [{"text": "Text"}]}, {"type": "heading-one", "children": [{"text": "Second Header 1"}]}, {"type": "paragraph", "children": [{"text": "Content 1 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "heading-two", "children": [{"text": "Second Header 2"}]}, {"type": "paragraph", "children": [{"text": "Content 2 - "}, {"bold": true, "text": "Various"}, {"text": " "}, {"text": "words and phrases", "italic": true}, {"text": " "}, {"text": "created", "strikethrough": true}, {"text": " brought to life "}, {"url": "https://google.com", "type": "link", "children": [{"text": "using"}]}, {"text": " a "}, {"text": "powerful", "underline": true}, {"text": " "}, {"code": true, "text": "rich text editor"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}];
 const wordToNum = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 };
 const numToWord = { 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine' };
 const sectionString = 'Header 2 > Non-existant Header > I doubt this works'//#'Second Header 1 >  Second Header 2';
 const contentToAdd = [{"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}, {"type": "paragraph", "children": [{"text": "This also has support for "}, {"bold": true, "text": "multiple paragraphs"}]}];
 const parsedSection = sectionString.split('>').map(sec => sec.trim());
 
+// Checks whether an element is a heading that matches some (optional) criteria
 const checkHeadingMatch = (element, targetText, targetHeadingSize) => {
   return (
     element.type.startsWith('heading-') && // check that it's a heading
@@ -13,17 +14,20 @@ const checkHeadingMatch = (element, targetText, targetHeadingSize) => {
   );
 };
 
-const insertElement = (data, headingSize, startingIndex=0) => {
-  let elementMatch;
+// Returns a new array with `data` inserted in the correct position
+export const insertElement = (data, headingSize=1, startingIndex=0) => {
+  let elementMatch; // this is the heading we are looking for
+
   for (let [index, element] of data.slice(startingIndex).entries()) {
     index += startingIndex;
+
     if (elementMatch) {
       // Match has been found
       if (checkHeadingMatch(element) || index === data.length - 1) {
+        // If the element is heading or end of data, we might insert the data
         const newHeadingSize = wordToNum[element.type.substring(8)];
-        console.log(headingSize, newHeadingSize, data)
+
         if (newHeadingSize <= headingSize || parsedSection.length === headingSize) {
-          console.log('inserting at', index, newHeadingSize, headingSize, parsedSection.length)
           // If the next same (or larger) level section has begun,
           // append the data right before it
           // OR
@@ -41,7 +45,6 @@ const insertElement = (data, headingSize, startingIndex=0) => {
             ...data.slice(index),
           ];
         } else if (index === data.length - 1) {
-          console.log('adding at end')
           // If this is the very last section,
           // append the data
           return [
@@ -49,20 +52,16 @@ const insertElement = (data, headingSize, startingIndex=0) => {
             ...contentToAdd,
           ];
         } else if (parsedSection[headingSize] === element.children[0].text) {
-          console.log('recursively searching')
           // If the new element is a subsection
           // that matches the next listed subsection
+          // recursively check it
           return insertElement(data, headingSize + 1, index - 1);
         };
       };
     } else {
       // Still looking for match
-      console.log(index, data.length - 1)
       if (checkHeadingMatch(element, parsedSection[headingSize - 1], headingSize)) {
         // Found a match
-        const size = element.type.substring(8);
-        const title = element.children[0].text;
-        console.log(title, wordToNum[size])
         elementMatch = element;
       } else if (
           (
@@ -84,5 +83,3 @@ const insertElement = (data, headingSize, startingIndex=0) => {
     };
   };
 };
-console.log(content)
-console.log(insertElement(content, 1));
--- alu-web/src/autonote/reader.js
+++ alu-web/src/autonote/reader.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {Button, Form} from 'react-bootstrap';
-import './adder';
+import './inserter';
 
 export function AutoNote(props) {
   const [selectedPar, setSelectedPar] = useState(0);
