790d1f2b39a92ac48bae8ee1db1d37c83a7e6d8a
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-09-28 15:16:45 -0400

    Added sentence-by-sentence reading to autonote

--- alu-web/src/notes/autonote/reader.js
+++ alu-web/src/notes/autonote/reader.js
@@ -16,6 +16,55 @@ const emptyValue = [
   },
 ];
 
+/*
+Tauris is a great and noble city, situated in a great province called IRAQ, in which are many other towns and villages. But as Tauris is the most noble I will tell you about it.
+The men of Tauris get their living by trade and handicrafts, for they weave many kinds of beautiful and valuable stuffs of silk and gold. The city has such a good position that merchandize is brought thither from India, Baudas, CREMESOR, and many other regions; and that attracts many Latin merchants, especially Genoese, to buy goods and transact other business there; the more as it is also a great market for precious stones. It is a city in fact where merchants make large profits.
+The people of the place are themselves poor creatures; and are a great medley of different classes. There are Armenians, Nestorians, Jacobites, Georgians, Persians, and finally the natives of the city themselves, who are worshippers of Mahommet. These last are a very evil generation; they are known as TAURIZI.] The city is all girt round with charming gardens, full of many varieties of large and excellent fruits.
+Now we will quit Tauris, and speak of the great country of Persia. [From Tauris to Persia is a journey of twelve days.]
+===
+In Turkey there are three classes of people. First, there are the Turcomans; these are worshippers of Mahommet, a rude people with an uncouth language of their own. They dwell among mountains and downs where they find good pasture, for their occupation is cattle-keeping. Excellent horses, known as Turquans, are reared in their country, and also very valuable mules. The other two classes are the Armenians and the Greeks, who live mixt with the former in the towns and villages, occupying themselves with trade and handicrafts. They weave the finest and handsomest carpets in the world, and also a great quantity of fine and rich silks of cramoisy and other colours, and plenty of other stuff. Their chief cities are CONIA, SAVAST [where the glorious Messer Saint Blaise suffered martyrdom], and CASARIA, besides many other towns and bishops' sees, of which we shall not speak at present, for it would be too long a matter. These people are subject to the Mongol of the Levant as their Suzerain.We will now leave this province, and speak of Greater Armenia.
+*/
+const parseText = (text, version='paragraph') => {
+  switch (version) {
+    case 'paragraph':
+      return text.split('\n');
+    case 'sentence':
+      const splitText = text.split(/\.\s/gm); // split by '.' followed by whitespace
+      const numSentences = 3; // maximum number of sentences to display at a time
+      let textArray = [];
+      let sentenceIndex = 0;
+
+      do {
+        const sentenceGroup = splitText.slice(sentenceIndex, sentenceIndex + numSentences);
+        
+        // Get sentences from the sentence group, unless it has a newline in it
+        let pair = [];
+        let newlinePairIndex;
+        for (const [i, sentence] of sentenceGroup.entries()) {
+          console.log(sentence, sentence.includes('\n'))
+          if (sentence.includes('\n')) {
+            newlinePairIndex = i;
+            break;
+          };
+          pair.push(sentence);
+        };
+
+        // Merge the sentences together
+        const mergedPair = pair.join('. ', '') + '.';
+        if (mergedPair.length > 1) {
+          textArray.push(mergedPair);
+        };
+
+        // Increase the sentence index by the number of sentences shown
+        sentenceIndex += newlinePairIndex ? newlinePairIndex : numSentences;
+      } while (sentenceIndex < splitText.length * numSentences);
+
+      return textArray;
+    default:
+      return;
+  };
+};
+
 export function AutoNote(props) {
   const [selectedPar, setSelectedPar] = useState(0);
   const [finished, setFinished] = useState(false);
@@ -31,12 +80,9 @@ export function AutoNote(props) {
     []
   );
 
-  const text = `
-Tauris is a great and noble city, situated in a great province called IRAQ, in which are many other towns and villages. But as Tauris is the most noble I will tell you about it.
-The men of Tauris get their living by trade and handicrafts, for they weave many kinds of beautiful and valuable stuffs of silk and gold. The city has such a good position that merchandize is brought thither from India, Baudas, CREMESOR, and many other regions; and that attracts many Latin merchants, especially Genoese, to buy goods and transact other business there; the more as it is also a great market for precious stones. It is a city in fact where merchants make large profits.
-The people of the place are themselves poor creatures; and are a great medley of different classes. There are Armenians, Nestorians, Jacobites, Georgians, Persians, and finally the natives of the city themselves, who are worshippers of Mahommet. These last are a very evil generation; they are known as TAURIZI.] The city is all girt round with charming gardens, full of many varieties of large and excellent fruits.
-Now we will quit Tauris, and speak of the great country of Persia. [From Tauris to Persia is a journey of twelve days.]
-`.trim().replace('\n\n', '\n').split('\n');
+const text = parseText(`
+In Turkey there are three classes of people. First, there are the Turcomans; these are worshippers of Mahommet, a rude people with an uncouth language of their own. They dwell among mountains and downs where they find good pasture, for their occupation is cattle-keeping. Excellent horses, known as Turquans, are reared in their country, and also very valuable mules. The other two classes are the Armenians and the Greeks, who live mixt with the former in the towns and villages, occupying themselves with trade and handicrafts. They weave the finest and handsomest carpets in the world, and also a great quantity of fine and rich silks of cramoisy and other colours, and plenty of other stuff. Their chief cities are CONIA, SAVAST [where the glorious Messer Saint Blaise suffered martyrdom], and CASARIA, besides many other towns and bishops' sees, of which we shall not speak at present, for it would be too long a matter. These people are subject to the Mongol of the Levant as their Suzerain.We will now leave this province, and speak of Greater Armenia.
+`.trim(), 'sentence');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -73,7 +119,7 @@ Now we will quit Tauris, and speak of the great country of Persia. [From Tauris 
     <div style={{ border: '1px solid gray', padding: '30px', height: '250px', overflow: 'hidden', borderRadius: '5px' }}>
       <p style={{ color: '#e0e0e0' }}>...{text[selectedPar - 1] && text[selectedPar - 1].substr(text[selectedPar - 1].length - 150, text[selectedPar - 1].length)}</p>
       <p style={{ color: '#000000' }}>{text[selectedPar]}</p>
-      <p style={{ color: '#e0e0e0' }}>...{text[selectedPar + 1] && text[selectedPar + 1].substr(0, 150)}</p>
+      <p style={{ color: '#e0e0e0' }}>{text[selectedPar + 1] && text[selectedPar + 1].substr(0, 150)}...</p>
     </div>
     {!finished ? <>
       <Form onSubmit={handleSubmit} className='mt-4'>
@@ -109,7 +155,7 @@ Now we will quit Tauris, and speak of the great country of Persia. [From Tauris 
         <Button type='submit' block>Add Notes</Button>
       </Form>
       </> :
-      <div className='mt-5'>
+      <div className='my-5'>
         <h3 className='text-center'>Here are the notes you took for this paper:</h3>
         <Slate
           editor={noteEditor}
