027dac3f6bdfdfc95f2285d8bbc505a47cf8b362
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2020-11-30 20:57:40 -0500

    Added error boundary to flashcard rendering

--- alu-web/src/decks/flashcards/detail.js
+++ alu-web/src/decks/flashcards/detail.js
@@ -7,91 +7,132 @@ import { Slate } from 'slate-react';
 import './detail.css';
 
 
+// At the time of coding creating error boundaries with hooks is not possible
+// Ideally when this functionality is added to React, this component would be updated
+class FlashCardRenderErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasFlash: false }
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  // componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    // logErrorToMyService(error, errorInfo);
+  // }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <p>Something went wrong. Please report this issue.</p>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+
 export function RenderFlashCardText(props) {
   const {flashcard, fixSlateLazy} = props;
-
+  
   const [frontValue, setFrontValue] = useState(flashcard.deck_fields[0].text);
   const frontEditor = useMemo(
     () => createFullEditor(),
     []
-  );
-  const [backValue, setBackValue] = useState(flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text);
-  const backEditor = useMemo(
-    () => createFullEditor(),
-    []
-  );
-
-  useEffect(() => {
-    if (fixSlateLazy) {
-      // Slate is lazy and won't automatically update the editor when the flashcard
-      // prop is changed, so we manually have to check if it has changed
-      // The frontValue dependency is excluded on purpose - including it causes infinite loop
-      if (flashcard.deck_fields[0].text !== frontValue || (flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text !== backValue)) {
-        setFrontValue(flashcard.deck_fields[0].text);
-        setBackValue(flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text);
+    );
+    const [backValue, setBackValue] = useState(flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text);
+    const backEditor = useMemo(
+      () => createFullEditor(),
+      []
+    );
+      
+    useEffect(() => {
+      try {
+        if (fixSlateLazy) {
+          // Slate is lazy and won't automatically update the editor when the flashcard
+          // prop is changed, so we manually have to check if it has changed
+          // The frontValue dependency is excluded on purpose - including it causes infinite loop
+          if (flashcard.deck_fields[0].text !== frontValue || (flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text !== backValue)) {
+            setFrontValue(flashcard.deck_fields[0].text);
+            setBackValue(flashcard.deck_fields.length > 1 && flashcard.deck_fields[1].text);
+          }
+        }
+      } catch (e) {
+        console.log(e);
       }
+      // eslint-disable-next-line
+    }, [flashcard]);
+      
+  try {
+      switch (flashcard.flashcard_type) {
+      case 'basic': case 'reversed': // two-sided
+        return (
+          <FlashCardRenderErrorBoundary>
+            <div className='col-md-6 text-center'>
+              <Slate
+                editor={frontEditor}
+                value={frontValue}
+                onChange={newValue => {
+                  setFrontValue(newValue);
+                }}
+              >
+                <FullEditor
+                  editor={frontEditor}
+                  readOnly={true}
+                  styleOptions={{ showBorder: false, minHeight: '0px' }}
+                />
+              </Slate>
+            </div>
+            <div className='col-md-6 text-center'>
+              <Slate
+                editor={backEditor}
+                value={backValue}
+                onChange={newValue => {
+                  setBackValue(newValue);
+                }}
+              >
+                <FullEditor
+                  editor={backEditor}
+                  readOnly={true}
+                  styleOptions={{ showBorder: false, minHeight: '0px' }}
+                />
+              </Slate>
+            </div>
+          </FlashCardRenderErrorBoundary>
+        )
+      case 'cloze': // one-sided
+        return (
+          <FlashCardRenderErrorBoundary>
+            <div className='col-md-12 text-center'>
+              <Slate
+                editor={frontEditor}
+                value={frontValue}
+                onChange={newValue => {
+                  setFrontValue(newValue);
+                }}
+              >
+                <FullEditor
+                  editor={frontEditor}
+                  readOnly={true}
+                  styleOptions={{ showBorder: false, minHeight: '0px' }}
+                />
+              </Slate>
+            </div>
+          </FlashCardRenderErrorBoundary>
+        );
+      default:
+        return (
+          <div className='col-md-12 text-center'>
+            <strong>Invalid flashcard type "{flashcard.flashcard_type}". Please report this issue.</strong>
+          </div>
+        );
     }
-    // eslint-disable-next-line
-  }, [flashcard]);
-
-  switch (flashcard.flashcard_type) {
-    case 'basic': case 'reversed': // two-sided
-      return (<>
-        <div className='col-md-6 text-center'>
-          <Slate
-            editor={frontEditor}
-            value={frontValue}
-            onChange={newValue => {
-              setFrontValue(newValue);
-            }}
-          >
-            <FullEditor
-              editor={frontEditor}
-              readOnly={true}
-              styleOptions={{ showBorder: false, minHeight: '0px' }}
-            />
-          </Slate>
-        </div>
-        <div className='col-md-6 text-center'>
-          <Slate
-            editor={backEditor}
-            value={backValue}
-            onChange={newValue => {
-              setBackValue(newValue);
-            }}
-          >
-            <FullEditor
-              editor={backEditor}
-              readOnly={true}
-              styleOptions={{ showBorder: false, minHeight: '0px' }}
-            />
-          </Slate>
-        </div>
-      </>)
-    case 'cloze': // one-sided
-      return (<>
-        <div className='col-md-12 text-center'>
-          <Slate
-            editor={frontEditor}
-            value={frontValue}
-            onChange={newValue => {
-              setFrontValue(newValue);
-            }}
-          >
-            <FullEditor
-              editor={frontEditor}
-              readOnly={true}
-              styleOptions={{ showBorder: false, minHeight: '0px' }}
-            />
-          </Slate>
-        </div>
-      </>);
-    default:
-      return (
-        <div className='col-md-12 text-center'>
-          <strong>Invalid flashcard type "{flashcard.flashcard_type}". Please report this issue.</strong>
-        </div>
-      );
+  } catch (e) {
+    console.log(e);
   }
 }
 
