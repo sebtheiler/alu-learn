fd8c987d7b6013ebaf3e84a1a9696f1dbfa1c758
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2021-12-29 10:40:08 -0500

    Added pro badge

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('is_pro', 'pro_expires',)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
--- alu-web/src/utils/utils.tsx
+++ alu-web/src/utils/utils.tsx
@@ -529,7 +529,6 @@ export function PasswordInput() {
           <i className={`fas fa-eye${showPassword ? '-slash' : ''} mx-auto`} />
         </InputGroup.Text>
       </InputGroup>
-      {/* @ts-expect-error */}
       <PasswordStrengthBar password={password} />
     </Form.Group>
   );
